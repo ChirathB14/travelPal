054a26762c9e9c5c0518c6cab40901d84f030c03
Nazma10 <nazmanazar5@gmail.com>
2023-02-07 11:51:16 +0530

    login

--- manage/manager/login.php
+++ manage/manager/login.php
@@ -74,7 +74,7 @@ require_once("../../inc/header.php");
 ?>
 
 <head>
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../../css/login.css">
 </head>
 
 <div class="login">
@@ -126,7 +126,7 @@ require_once("../../inc/header.php");
 </script>
 
 <?php
-require_once("../inc/footer.php");
+require_once("../../inc/footer.php");
 ?>
 
 <?php mysqli_close($connection); ?>
