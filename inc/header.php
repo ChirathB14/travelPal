3b1fe7f8fea7be9052677e3b693e61c4cace72b1
Nazma10 <nazmanazar5@gmail.com>
2023-02-07 11:47:03 +0530

    Create Tour Plan page

--- inc/header.php
+++ inc/header.php
@@ -29,8 +29,8 @@
                 <div class=\"menu\">
                     <button class=\"nav\" onclick=\"location.href = '/travelPal/index.php';\">HOME</button>
                     <button class=\"nav\"onclick=\"location.href = '/travelPal/TourPlan.php';\">TOUR PLAN</button>
-                    <button class=\"nav\" onclick=\"location.href = './contactus.php';\">CONTACT US</button>
-                    <button class=\"nav\"onclick=\"location.href = './Blogs.php';\">BLOGS</button>
+                    <button class=\"nav\" onclick=\"location.href = '/travelPal/contactus.php';\">CONTACT US</button>
+                    <button class=\"nav\"onclick=\"location.href = '/travelPal/Blogs.php';\">BLOGS</button>
                     <button class=\"logout-btn\" onclick=\"location.href = '/travelPal/register.php';\" >REGISTER</button>
                     <button class=\"logout-btn\" onclick=\"location.href = './login.php';\" ><i class=\"fa fa-user fa-lg\" aria-hidden=\"true\"></i>&nbsp;&nbsp;LOG IN</button>
                 </div>            
@@ -49,7 +49,7 @@
                 <div class=\"menu\">
                     <button class=\"nav\" onclick=\"location.href = '/travelPal/index.php';\">HOME</button>
                     <button class=\"nav\"onclick=\"location.href = '/travelPal/tourplan-pre.php';\">TOUR PLAN</button>
-                    <button class=\"nav\"onclick=\"location.href = 'contactus.php';\">CONTACT US</button>
+                    <button class=\"nav\"onclick=\"location.href = '/travelPalcontactus.php';\">CONTACT US</button>
                     <button class=\"nav\"onclick=\"location.href = '/travelPal/Blogs.php';\">BLOGS</button>
                     <button class=\"nav\">PROFILE</button>
                     <button class=\"logout-btn\" onclick=\"location.href = 'logout.php';\" ><i class=\"fa fa-user fa-lg\" aria-hidden=\"true\"></i>&nbsp;&nbsp;LOG OUT</button>
