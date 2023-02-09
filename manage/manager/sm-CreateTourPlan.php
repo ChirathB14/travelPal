43672a44281eed9bdec1147b476f16b50b5b214f
ChirathB14 <chirathb14@gmail.com>
2023-02-09 05:59:35 +0530

    Minor updates

--- manage/manager/sm-CreateTourPlan.php
+++ manage/manager/sm-CreateTourPlan.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $errors = array_merge($errors, check_max_length($max_len_fields));
 
     if (empty($errors)) {
-        $tourplanID = rand(0,100);
+        $tourplanID = rand(0, 100);
         $sql = "INSERT INTO premadetourplan(tourPlanID,budget,userID,season,location,noOfDays,type)
                 VALUES ($tourplanID, $Budget,$user_id,'$season','$Location',$No_of_Days,'$Type_of_Package');";
 
@@ -59,10 +59,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-//     $sql = "INSERT INTO premadetourplan(budget,userID,season,location,noOfDays,type)
-// VALUES ($Budget,$user_id,'$season','$Location',$No_of_Days,'$Type_of_Package');";
+    //     $sql = "INSERT INTO premadetourplan(budget,userID,season,location,noOfDays,type)
+    // VALUES ($Budget,$user_id,'$season','$Location',$No_of_Days,'$Type_of_Package');";
 
-//     $result = mysqli_query($connection, $sql);
+    //     $result = mysqli_query($connection, $sql);
 
 }
 
@@ -73,12 +73,12 @@ require_once "../../inc/header.php";
 ?>
 
 <head>
-        <link rel="stylesheet" href="/travelPal/css/main.css" type="text/css">
+    <link rel="stylesheet" href="/travelPal/css/main.css" type="text/css">
 </head>
 
 <div class="body">
-        <!-- Profile page content -->
-        <div class="page-content">
+    <!-- Profile page content -->
+    <div class="page-content">
         <!-- Dashboard - Site Manager -->
         <div class="Dashboard">
             <div class="Dashboard-top">
@@ -96,76 +96,76 @@ require_once "../../inc/header.php";
             </div>
         </div>
 
-        
-        <div class="content"> 
-        <h2>Create New Plan</h2>
-                <?php
-                    if (!empty($errors)) {
-                        display_errors($errors);
-                    }
-                ?>
-                <?php
-                if (isset($_GET['success'])) {
-                    echo '<p class="info">Tour plan added successfully.</p>';
-                }
-                ?>
-                <?php
-                if (isset($_GET['failed'])) {
-                    echo "<p class='error'> Failed to add the new record. Error: " .mysqli_error($connection)."</p>";
-                }
-                ?>
 
-        <div class="profile-content">
-        <form action="" class="form-update" method='post'>
-            <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
-                <div class="details-update" style="margin-bottom: 11px">
-                    <p>
-                        <select id="" name="season" style="width: 340px;  margin-top: 7px; 
+        <div class="content">
+            <h2>Create New Plan</h2>
+            <?php
+            if (!empty($errors)) {
+                display_errors($errors);
+            }
+            ?>
+            <?php
+            if (isset($_GET['success'])) {
+                echo '<p class="info">Tour plan added successfully.</p>';
+            }
+            ?>
+            <?php
+            if (isset($_GET['failed'])) {
+                echo "<p class='error'> Failed to add the new record. Error: " . mysqli_error($connection) . "</p>";
+            }
+            ?>
+
+            <div class="profile-content">
+                <form action="" class="form-update" method='post'>
+                    <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+                    <div class="details-update" style="margin-bottom: 11px">
+                        <p>
+                            <select id="" name="season" style="width: 340px;  margin-top: 7px; 
                             background-color: var(--accentcolor); height: 34px;
                             border: none; font-size: 12px; font-weight: bold;">
-                            <option value="" disabled selected>SELECT A SEASON</option>
-                            <option value="NOVEMBER-MARCH">NOVEMBER-MARCH</option>
-                            <option value="APRIL-JUNE">APRIL-JUNE</option>
-                            <option value="JULY-OCTOMBER">JULY-OCTOMBER</option>
-                        </select>
-                    </p> 
-                </div>
-                <div class="details-update">
-                    <p>
-                        &nbsp; Location :
-                        <input type="text" placeholder="Location" name="Location" value="<?php echo $Location ; ?>" required>
-                    </p>
-                </div>
-                <div class="details-update">
-                    <p>
-                        &nbsp; No of Nights : 
-                        <input type="number" placeholder="No of Nights" name="No_of_Nights" value="<?php echo $No_of_Nights ; ?>" required min=0> 
-                    </p> 
-                </div>
-                <div class="details-update">
-                    <p>
-                        &nbsp; Budget :
-                        <input type="text" placeholder="Budget" name="Budget" value="<?php echo $Budget ; ?>" required> 
-                    </p> 
-                </div>
-                <div class="details-update" style="margin-bottom: 11px">
-                    <p>
-                        <select id="" name="season" style="width: 340px;  margin-top: 7px; 
+                                <option value="" disabled selected>SELECT A SEASON</option>
+                                <option value="NOVEMBER-MARCH">NOVEMBER-MARCH</option>
+                                <option value="APRIL-JUNE">APRIL-JUNE</option>
+                                <option value="JULY-OCTOMBER">JULY-OCTOMBER</option>
+                            </select>
+                        </p>
+                    </div>
+                    <div class="details-update">
+                        <p>
+                            &nbsp; Location :
+                            <input type="text" placeholder="Location" name="Location" value="<?php echo $Location; ?>" required>
+                        </p>
+                    </div>
+                    <div class="details-update">
+                        <p>
+                            &nbsp; No of Nights :
+                            <input type="number" placeholder="No of Nights" name="No_of_Nights" value="<?php echo $No_of_Nights; ?>" required min=0>
+                        </p>
+                    </div>
+                    <div class="details-update">
+                        <p>
+                            &nbsp; Budget :
+                            <input type="text" placeholder="Budget" name="Budget" value="<?php echo $Budget; ?>" required>
+                        </p>
+                    </div>
+                    <div class="details-update" style="margin-bottom: 11px">
+                        <p>
+                            <select id="" name="season" style="width: 340px;  margin-top: 7px; 
                             background-color: var(--accentcolor); height: 34px;
                             border: none; font-size: 12px; font-weight: bold;">
-                            <option value="" disabled selected>TYPE OF PACKAGE</option>
-                            <option value="Luxury Tours">Luxury Tours</option>
-                            <option value="Budget Tours">Budget Tours</option>
-                            <option value="Family Tours">Family Tours</option>
-                            <option value="Honeymoon Tours">Honeymoon Tours</option>
-                        </select>
-                    </p> 
-                </div>
-                <button Type="submit">Create Tour Plan</button>
-        </form>
+                                <option value="" disabled selected>TYPE OF PACKAGE</option>
+                                <option value="Luxury Tours">Luxury Tours</option>
+                                <option value="Budget Tours">Budget Tours</option>
+                                <option value="Family Tours">Family Tours</option>
+                                <option value="Honeymoon Tours">Honeymoon Tours</option>
+                            </select>
+                        </p>
+                    </div>
+                    <button Type="submit">Create Tour Plan</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
-<?php require_once "../../inc/footer.php";?>
-<?php mysqli_close($connection); ?>
+    <?php require_once "../../inc/footer.php"; ?>
+    <?php mysqli_close($connection); ?>
