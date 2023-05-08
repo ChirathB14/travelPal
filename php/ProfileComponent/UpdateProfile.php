6f5605b307b7dc289ee1459255e0f82be9217798
Nazma10 <nazmanazar5@gmail.com>
2023-05-08 18:41:44 +0530

    Service Provider updates

--- php/ProfileComponent/UpdateProfile.php
+++ php/ProfileComponent/UpdateProfile.php
@@ -5,15 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/header.css">
-    <link rel="stylesheet" href="../../css/profile.css">
-    <link rel="stylesheet" href="../../css/newFooter.css">
-    <script type="text/javascript" src="../../js/profile.js"></script>
 
-    <title>Travel Pal</title>
+
+    <!-- <link rel="stylesheet" href="../../css/header.css"> -->
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="stylesheet" href="../../css/profile.css">
+    <!-- <link rel="stylesheet" href="../../css/newFooter.css"> -->
+    <script type="text/javascript" src="../../js/profile.js"></script>
 </head>
 
-<body style="background-color: #0E064D;" onload="checkUserAccess()">
+
+<?php
+    $title = "Update Profile - TravePal";
+?>
+
+<body onload="checkUserAccess()">
     <?php
     require '../DbConfig.php';
     if (isset($_COOKIE['user'])) {
@@ -29,6 +35,29 @@
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
     ?>
+
+<div class="header">
+            <div class="navigationbar">
+                <div class="nav-Logo">
+                    <a href="/travelPal/index.php">
+                        <img src="/travelPal/images/logo.png" alt="TRAVELPal">
+                    </a>
+                </div>
+                <div class="menu">
+                    <button class="nav" onclick="location.href = '/travelPal/index.php';">HOME</button>
+                    <button class="nav"onclick="location.href = '/travelPal/php/TourPlanningComponent/TourPlanningIndex.php';">TOUR PLAN</button>
+                    <button class="nav" onclick="location.href = '/travelPal/php/Blog/ContactUS.php';">CONTACT US</button>
+                    <button class="nav"onclick="location.href = '/travelPal/php/Blog/ViewBlogs.php';">BLOGS</button>
+                    <button class="nav"onclick="location.href = '/travelPal/php/ProfileComponent/Profile.php';">PROFILE</button>
+                    <button class="logout-btn" id="logout" onclick="logOut()"><i class="fa fa-user fa-lg" aria-hidden="true"></i>&nbsp;&nbsp;LOG OUT</button>
+                </div>            
+            </div>
+            <div class="navigationbarfoot">
+                <hr>  
+            </div>    
+        </div>
+
+    <!--
                     <ul class="header-ul">
                         <li class="header-left-li"><img class="headerbtm" src="../../images/logo.png" alt="logo" width="150" height="50"></li>
                         <li class="header-left-li"><a class="header-left-li a" href="../../index.php">Home</a></li>
@@ -39,11 +68,13 @@
                         <li class="header-right-li"><a class="header-left-li a" id="logout"><button class="button-login" onclick="logOut()"><img src="../../images/User-Icon.png" alt="logo" width="20" height="20" style="margin-right: 10px;">Logout</button></a></li>
                     </ul>
                     <hr style="background-color: #327972;color:#327972"/>
+                --> 
+
                     <table style="width:100%;height: 100%;overflow-y: hidden;">
                         <tr VALIGN=TOP>
                         <?php include './subComponent/VerticleHeader.php'; ?>
                             <td class="td-profile">
-                                <div class="main-wrapper">
+                                <div class="main-wrapper" style="margin-top: 10px;">
                                     <h2 class="heder-profile">Update Profile</h2>
                                     <div class="profile-main-wrapper">
                                         <!-- <form method="POST" action="UpdateProfile.php">
@@ -67,20 +98,20 @@
                                         ?>
                                         <form method="POST" action="UpdateProfile.php">
                                             <div class="line-wrapper">
-                                                <label class="line-txt" for="firstName"><b>First Name:</b></label>
-                                                <input class="line-wrapper line-txt" type="text" id="firstName" name="firstName" style="width:90%" placeholder="First Name" value="<?php echo $row['first_name']; ?>" required>
+                                                <label class="line-txt" for="firstName" style="width:25%"><b>First Name:</b></label>
+                                                <input class="line-wrapper line-txt" type="text" id="firstName" name="firstName" style="width:75%" placeholder="First Name" value="<?php echo $row['first_name']; ?>" required>
                                             </div>
                                             <div class="line-wrapper">
-                                                <label class="line-txt" for="lastName"><b>Last Name:</b></label>
-                                                <input class="line-wrapper line-txt" type="text" id="lastName" name="lastName" style="width:90%" placeholder="Last Name" value="<?php echo $row['last_name']; ?>" required>
+                                                <label class="line-txt" for="lastName" style="width:25%"><b>Last Name:</b></label>
+                                                <input class="line-wrapper line-txt" type="text" id="lastName" name="lastName" style="width:75%" placeholder="Last Name" value="<?php echo $row['last_name']; ?>" required>
                                             </div>
                                             <div class="line-wrapper">
-                                                <label class="line-txt" for="email"><b>Email:</b></label>
-                                                <input class="line-wrapper line-txt" type="email" id="email" name="email" style="width:90%" placeholder="Email" value="<?php echo $row['email']; ?>" required>
+                                                <label class="line-txt" for="email" style="width:25%"><b>Email:</b></label>
+                                                <input class="line-wrapper line-txt" type="email" id="email" name="email" style="width:75%" placeholder="Email" value="<?php echo $row['email']; ?>" required>
                                             </div>
                                             <div class="line-wrapper">
-                                                <label class="line-txt" for="address"><b>Address:</b></label>
-                                                <input class="line-wrapper line-txt" type="text" id="address" name="address" style="width:90%" placeholder="Address" value="<?php echo $row['address']; ?>" required>
+                                                <label class="line-txt" for="address" style="width:25%"><b>Address:</b></label>
+                                                <input class="line-wrapper line-txt" type="text" id="address" name="address" style="width:75%" placeholder="Address" value="<?php echo $row['address']; ?>" required>
                                             </div>
                                             <center>
                                                 <button class="update-btn" type="submit" id="updateBtn" name="updateBtn" value="updateBtn">Save</button>
@@ -111,9 +142,6 @@
 
                                             ?>
                                         </form>
-
-
-
                                     </div>
                                 </div>
                             </td>
@@ -134,6 +162,8 @@
     }
     ?>
 </body>
+
+<!--
 <footer class="custom-footer">
     <div class="footer-left">
         <img src="../../images/logo.png" alt="Company logo" class="footer-logo">
@@ -167,4 +197,9 @@
         <img src="../../images/footerimg.png" alt="Image description" class="footer-image">
     </div>
 </footer>
+--> 
+    
+<!-- footer -->
+<?php require_once("../Common/footer.php");?>
+
 </html>
--- php/ProfileComponent/ViewAvailability.php
+++ php/ProfileComponent/ViewAvailability.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/header.css">
+
+    
+    <!-- <link rel="stylesheet" href="../../css/header.css"> -->
     <link rel="stylesheet" href="../../css/profile.css">
-    <link rel="stylesheet" href="../../css/newFooter.css">
+    <link rel="stylesheet" href="../../css/main.css">
+    <!-- <link rel="stylesheet" href="../../css/newFooter.css"> -->
     <script type="text/javascript" src="../../js/profile.js"></script>
-
-
-    <title>Travel Pal</title>
 </head>
 
-<body style="background-color: #0E064D;" onload="checkUserAccess()">
+<body onload="checkUserAccess()">
     <?php
     require '../DbConfig.php';
     if (isset($_COOKIE['user'])) {
@@ -30,6 +30,29 @@
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
     ?>
+
+<div class="header">
+            <div class="navigationbar">
+                <div class="nav-Logo">
+                    <a href="/travelPal/index.php">
+                        <img src="/travelPal/images/logo.png" alt="TRAVELPal">
+                    </a>
+                </div>
+                <div class="menu">
+                    <button class="nav" onclick="location.href = '/travelPal/index.php';">HOME</button>
+                    <button class="nav"onclick="location.href = '/travelPal/php/TourPlanningComponent/TourPlanningIndex.php';">TOUR PLAN</button>
+                    <button class="nav" onclick="location.href = '/travelPal/php/Blog/ContactUS.php';">CONTACT US</button>
+                    <button class="nav"onclick="location.href = '/travelPal/php/Blog/ViewBlogs.php';">BLOGS</button>
+                    <button class="nav"onclick="location.href = '/travelPal/php/ProfileComponent/Profile.php';">PROFILE</button>
+                    <button class="logout-btn" id="logout" onclick="logOut()"><i class="fa fa-user fa-lg" aria-hidden="true"></i>&nbsp;&nbsp;LOG OUT</button>
+                </div>            
+            </div>
+            <div class="navigationbarfoot">
+                <hr>  
+            </div>    
+        </div>
+
+    <!--
                     <ul class="header-ul">
                         <li class="header-left-li"><img class="headerbtm" src="../../images/logo.png" alt="logo" width="150" height="50"></li>
                         <li class="header-left-li"><a class="header-left-li a" href="../../index.php">Home</a></li>
@@ -40,6 +63,8 @@
                         <li class="header-right-li"><a class="header-left-li a" id="logout"><button class="button-login" onclick="logOut()"><img src="../../images/User-Icon.png" alt="logo" width="20" height="20" style="margin-right: 10px;">Logout</button></a></li>
                     </ul>
                     <hr style="background-color: #327972;color:#327972"/>
+                --> 
+
                     <table style="width:100%">
                         <tr VALIGN=TOP>
                             <?php include './subComponent/VerticleHeader.php'; ?>
