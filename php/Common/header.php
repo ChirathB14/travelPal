992212364c8a7f9ecaada93440f43bc0af0ccc0d
Nazma10 <nazmanazar5@gmail.com>
2023-05-09 15:59:54 +0530

    Latest Updates

--- php/Common/header.php
+++ php/Common/header.php
@@ -69,22 +69,6 @@
         </div> ";
     }
     ?>
-
-
-    <!--
-    <ul class="header-ul">
-        <li class="header-left-li"><img class="headerbtm" src="../../images/logo.png" alt="logo" width="150" height="50"></li>
-        <li class="header-left-li"><a class="header-left-li a" href="../../index.php">Home</a></li>
-        <li class="header-left-li"><a class="header-left-li a" href="../TourPlanningComponent/TourPlanningIndex.php">Tour Plan</a></li>
-        <li class="header-left-li"><a class="header-left-li a" href="../Blog/ContactUS.php">Contact Us</a></li>
-        <li class="header-left-li"><a class="header-left-li a" href="../Blog/ViewBlogs.php">Blogs</a></li>
-        <li class="header-left-li"><a class="header-left-li a" id="profile" href="../../php/ProfileComponent/Profile.php">Profile</a></li>
-        <li class="header-right-li"><a class="header-left-li a" id="login" href="../../php/Login.php"><button class="button-login" href="../Blog/ViewBlogs.php"><img src="../../images/User-Icon.png" alt="logo" width="20" height="20" style="margin-right: 10px;">Login</button></a></li>
-        <li class="header-right-li"><a class="header-left-li a" id="register" href="../../php/RegisterUser.php"><button class="button-register" href="../Blog/ViewBlogs.php"><img src="../../images/User-Icon.png" alt="logo" width="20" height="20" style="margin-right: 10px;">Register</button></a></li>
-        <li class="header-right-li"><a class="header-left-li a" id="logout" onclick="logOut()"><button class="button-login" href="../Blog/ViewBlogs.php"><img src="../../images/User-Icon.png" alt="logo" width="20" height="20" style="margin-right: 10px;">Logout</button></a></li>
-    </ul>
-    <hr style="background-color: #327972;color:#327972" />
--->
 </body>
 
 </html>
--- php/ProfileComponent/UpdateProfile.php
+++ php/ProfileComponent/UpdateProfile.php
@@ -6,12 +6,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
-    <!-- <link rel="stylesheet" href="../../css/header.css"> -->
     <link rel="stylesheet" href="../../css/main.css">
     <link rel="stylesheet" href="../../css/profile.css">
-    <!-- <link rel="stylesheet" href="../../css/newFooter.css"> -->
+
     <script type="text/javascript" src="../../js/profile.js"></script>
+
+    <style>
+        .update {
+            display: flex;
+            flex-direction: row;
+            background-color: var(--accentcolor);
+            color: var(--primarycolor);
+            border-radius: 5px;
+            border: none;
+            cursor: pointer;
+            margin: 10px;
+        }
+
+        .update label {
+            font-size: 14px;
+            font-weight: 500;
+            margin: 25px 10px 25px 15px;
+        }
+
+        .update input {
+            font-size: 14px;
+            font-weight: 500;
+            margin: 15px;
+            border-radius: 5px;
+            border: 2px solid gray;
+            padding: 5px 10px;
+        }
+    </style>
 </head>
 
 
@@ -56,19 +82,6 @@
                 <hr>  
             </div>    
 </div>
-        
-                    <!-- <ul class="header-ul">
-
-                        <li class="header-left-li"><img class="headerbtm" src="../../images/logo.png" alt="logo" width="150" height="50"></li>
-                        <li class="header-left-li"><a class="header-left-li a" href="../../index.php">Home</a></li>
-                        <li class="header-left-li"><a class="header-left-li a" href="../TourPlanningComponent/TourPlanningIndex.php">Tour Plan</a></li>
-                        <li class="header-left-li"><a class="header-left-li a" href="../Blog/ContactUS.php">Contact Us</a></li>
-                        <li class="header-left-li"><a class="header-left-li a" href="../Blog/ViewBlogs.php">Blogs</a></li>
-                        <li class="header-left-li"><a class="header-left-li a" style="background-color: #00357A;" id="profile" href="./Profile.php">Profile</a></li>
-                        <li class="header-right-li"><a class="header-left-li a" id="logout"><button class="button-login" onclick="logOut()"><img src="../../images/User-Icon.png" alt="logo" width="20" height="20" style="margin-right: 10px;">Logout</button></a></li>
-                    </ul> 
-                    <hr style="background-color: #327972;color:#327972"/>
-                --> 
 
                     <table style="width:100%; height: 100%; overflow-y: hidden;">
                         <tr VALIGN=TOP>
@@ -77,9 +90,7 @@
                                 <div class="main-wrapper" style="margin-top: 10px;">
                                     <h2 class="heder-profile">Update Profile</h2>
                                     <div class="profile-main-wrapper">
-                                        <!-- <form method="POST" action="UpdateProfile.php">
-                                            <button class="delete-btn" id="deleteBtn" name="deleteBtn">xxx</button>
-                                        </form> -->
+
                                         <?php
                                         if (isset($_POST['deleteBtn'])) {
                                             $sql = "DELETE FROM user WHERE user_Id= '$userID'";
@@ -97,19 +108,19 @@
                                         }
                                         ?>
                                         <form method="POST" action="UpdateProfile.php">
-                                            <div class="line-wrapper">
+                                            <div class="update">
                                                 <label class="line-txt" for="firstName" style="width:25%"><b>First Name:</b></label>
                                                 <input class="line-wrapper line-txt" type="text" id="firstName" name="firstName" style="width:75%" placeholder="First Name" value="<?php echo $row['first_name']; ?>" required>
                                             </div>
-                                            <div class="line-wrapper">
+                                            <div class="update">
                                                 <label class="line-txt" for="lastName" style="width:25%"><b>Last Name:</b></label>
                                                 <input class="line-wrapper line-txt" type="text" id="lastName" name="lastName" style="width:75%" placeholder="Last Name" value="<?php echo $row['last_name']; ?>" required>
                                             </div>
-                                            <div class="line-wrapper">
+                                            <div class="update">
                                                 <label class="line-txt" for="email" style="width:25%"><b>Email:</b></label>
                                                 <input class="line-wrapper line-txt" type="email" id="email" name="email" style="width:75%" placeholder="Email" value="<?php echo $row['email']; ?>" required>
                                             </div>
-                                            <div class="line-wrapper">
+                                            <div class="update">
                                                 <label class="line-txt" for="address" style="width:25%"><b>Address:</b></label>
                                                 <input class="line-wrapper line-txt" type="text" id="address" name="address" style="width:75%" placeholder="Address" value="<?php echo $row['address']; ?>" required>
                                             </div>
@@ -161,41 +172,6 @@
     }
     ?>
 </body>
-
-<!-- <footer class="custom-footer">
-    <div class="footer-left">
-        <img src="../../images/logo.png" alt="Company logo" class="footer-logo">
-        <div class="footer-title">
-            <h3 class="footer-heading">Get inspired ! Recieve travel discounts, tips & behind the scene stories</h3>
-        </div>
-        <form class="footer-form">
-            <input type="text" class="footer-input" placeholder="Enter your email address">
-            <button type="submit" class="footer-button">Subscribe</button>
-        </form>
-        <table style="width: 100%;margin-top:20px">
-            <tr>
-                <td class="footer-td-text">HOME</td>
-                <td class="footer-td-text">ABOUT US</td>
-                <td class="footer-td-text">CONTACT US</td>
-            </tr>
-            <tr>
-                <td class="footer-td-text">BLOGs</td>
-                <td class="footer-td-text">Tour plans</td>
-                <td class="footer-td-text">Preplanned Tour</td>
-            </tr>
-            <tr>
-                <td class="footer-td-text">Customize Tour</td>
-                <td class="footer-td-text">BLOGs</td>
-                <td class="footer-td-text">Create Blogs</td>
-            </tr>
-        </table>
-
-    </div>
-    <div class="footer-right">
-        <img src="../../images/footerimg.png" alt="Image description" class="footer-image">
-    </div>
-</footer>
---> 
     
 <footer>
         <hr>
