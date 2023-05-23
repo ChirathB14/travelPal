d98d7618e2b1a13d0b9ccc046b9d14c6da7f2cd1
Nazma10 <nazmanazar5@gmail.com>
2023-05-23 13:37:31 +0530

    Forgot Password minor changes

--- php/fogotPassword/verify_otp.php
+++ php/fogotPassword/verify_otp.php
@@ -1,5 +1,52 @@
 <head>
+    
+    <link rel="stylesheet" href="../../css/main.css">
     <script type="text/javascript" src="../../js/profile.js"></script>
+
+    <style>
+        .container {
+            max-width: 500px;
+            margin: 100px auto;
+            padding: 20px;
+            background-color: rgba(0, 53, 122, 0.5);
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 53, 122, 0.5);
+        }
+
+        label {
+            margin-top: 10px;
+            font-weight: 600;
+            font-size: 14px;
+            line-height: 30px;
+            letter-spacing: 0.1em;
+            color: var(--accentcolor);
+        }
+
+        input[type="email"],
+        input[type="password"],
+        input[type="text"],
+        input[type="number"] {
+            padding: 10px;
+            margin: 5px 0;
+            background: rgba(255, 255, 255, 0.75);
+            border: 1px solid #FFFFFF;
+            border-radius: 15px;
+            height: 40px;
+            width: 100%;
+        }
+
+        input[type="submit"] {
+            background: var(--secondarycolor);
+            border-radius: 15px;
+            padding: 10px;
+            margin: 12px 0;
+            font-weight: 600;
+            font-size: 16px;
+            color: #FFFFFF;
+            height: 50px;
+            width: 100%;
+        }
+    </style>
 </head>
 
 <?php
@@ -62,21 +109,24 @@ if (isset($_GET['email'])) {
 
 ?>
 
-<p>
-    An OTP has been sent to your email address. Please enter it below to reset your password.
-</p>
-<form action="verify_otp.php" method="POST">
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" value="<?php echo $email; ?>" readonly>
+<div class="container">
+    <p>
+        An OTP has been sent to your email address. Please enter it below to reset your password.
+    </p>
     <br>
-    <label for="password">New Password</label>
-    <input type="password" name="password" id="password" placeholder="***********" required>
-    <br>
-    <label for="confirm_password">Re enter the same password</label>
-    <input type="password" name="confirm_password" id="confirm_password" placeholder="***********" required>
-    <br>
-    <label for="otp">OTP</label>
-    <input type="number" id="otp" name="otp" required>
-    <br>
-    <input type="submit" value="Submit">
-</form>';
+    <form action="verify_otp.php" method="POST">
+        <label for="email">Email :</label> <br>
+        <input type="text" name="email" id="email" value="<?php echo $email; ?>" readonly>
+        <br>
+        <label for="password">New Password :</label> <br>
+        <input type="password" name="password" id="password" placeholder="***********" required>
+        <br>
+        <label for="confirm_password">Re enter the same password :</label> <br>
+        <input type="password" name="confirm_password" id="confirm_password" placeholder="***********" required>
+        <br>
+        <label for="otp">OTP :</label> <br>
+        <input type="number" id="otp" name="otp" required>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
